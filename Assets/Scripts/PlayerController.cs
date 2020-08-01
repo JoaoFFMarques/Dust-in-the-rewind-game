a82923f7270594256e50651ff5863ef212ae22a1
JoaoFFMarques <joao.fernando@fatec.sp.gov.br>
2020-08-01 13:26:43 -0300

    player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody m_PlayerRB;
+    private Vector3 m_Movement;
+    public float m_MoveSpeedy;
+
+
+    private bool m_IsLookingLeft;
+    
+
+    void Start()
+    {
+        m_PlayerRB = GetComponent<Rigidbody>();       
+    }
+
+    void Update()
+    {
+       
+           
+        
+    }
+   
+    private void FixedUpdate()
+    {
+        Move();          
+       
+    }
+   
+    private void Move()
+    {
+        m_Movement.x = Input.GetAxis("Horizontal");
+        m_Movement.y = Input.GetAxis("Vertical");
+
+        if(m_Movement.x > 0 && m_IsLookingLeft)
+            Rotate();
+        else if(m_Movement.x < 0 && !m_IsLookingLeft)
+            Rotate();
+
+        m_PlayerRB.MovePosition(m_PlayerRB.position + m_Movement * m_MoveSpeedy * Time.fixedDeltaTime);
+
+    }
+    private void Rotate()
+    {
+        float x = transform.localScale.x * -1;
+
+        m_IsLookingLeft = !m_IsLookingLeft;
+
+        transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+    }
+   
+    
+   
+}

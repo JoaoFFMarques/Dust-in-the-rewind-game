9007599023e65e30a2bc719e35512f0576aca92d
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2020-08-02 11:24:32 -0300

    Bug fix continue and settings

---
+++ Assets/Scripts/UIs/Behaviours/ContinueButtonUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContinueButtonUI : MonoBehaviour
+{
+    private Selectable m_Selectable;
+    public string m_Key = "continue";
+    public bool m_UseDelete = true;
+
+    private void Awake()
+    {
+        m_Selectable = GetComponent<Selectable>();
+    }
+
+    private void OnEnable()
+    {
+        if (m_UseDelete)
+            Destroy(gameObject);
+        else
+            m_Selectable.interactable = PlayerPrefs.HasKey(m_Key);
+    }
+}
